235703d875e53f4acbacc218af9b9446fb1ec5fe
Christian Berger <c.berger18@gmail.com>
2018-10-09 21:15:37 +0200

    Added NuGet Package for eZet.EveLib.Core and EveCrest

---
+++ Eve-FastIntel/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+	Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs)
+
+	End Sub
+
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+		Me.Close()
+
+
+	End Sub
+
+End Class

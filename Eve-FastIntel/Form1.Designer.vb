81db9466858bccdb88ef4f1d373c6dffb78f2c0e
Christian Berger <c.berger18@gmail.com>
2018-10-11 23:33:11 +0200

    Added ICO as Form and Application Symbol

--- Eve-FastIntel/Form1.Designer.vb
+++ Eve-FastIntel/Form1.Designer.vb
@@ -231,6 +231,7 @@ Partial Class Form1
 		Me.Controls.Add(Me.Button2)
 		Me.Controls.Add(Me.Button1)
 		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
 		Me.MaximizeBox = False
 		Me.MinimizeBox = False
 		Me.Name = "Form1"
